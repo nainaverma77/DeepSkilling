a6d83a46ab80a47feb183a0f2915b44e23e92f5d
Naina Verma <nainaverma6080@gmail.com>
2026-07-05 13:52:54 +0530

    Exercise-2 Spring Core - Load Country from Spring Configuration XML

---
+++ 1.2 spring rest handson/spring-learn/src/main/java/com/example/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,31 @@
+package com.example.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		displayDate();
+	}
+
+	public static void displayDate() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		try {
+			Date date = format.parse("31/12/2018");
+			System.out.println("Parsed Date: " + date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
