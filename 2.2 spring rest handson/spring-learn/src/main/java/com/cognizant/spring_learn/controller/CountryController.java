e12acb0101c28e4eefe430009a3a3dd2d4136745
Naina Verma <nainaverma6080@gmail.com>
2026-07-07 19:08:45 +0530

    Exercise-2 REST - Country Web Service

---
+++ 2.2 spring rest handson/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,20 @@
+package com.cognizant.spring_learn.controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.Country;
+
+@RestController
+public class CountryController {
+
+    @RequestMapping(value = "/country", method = RequestMethod.GET)
+    public Country getCountryIndia() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("in", Country.class);
+        return country;
+    }
+}
