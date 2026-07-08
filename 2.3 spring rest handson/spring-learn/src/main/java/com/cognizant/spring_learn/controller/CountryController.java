c2c92bada13dbf9805320f1a651df4adc69cd08a
Naina Verma <nainaverma6080@gmail.com>
2026-07-08 16:12:23 +0530

    Exercise-3 REST - Get country based on country code

---
+++ 2.3 spring rest handson/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,31 @@
+package com.cognizant.spring_learn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.Country;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private com.cognizant.spring_learn.service.CountryService countryService;
+
+    @RequestMapping(value = "/country", method = RequestMethod.GET)
+    public Country getCountryIndia() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("in", Country.class);
+        return country;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+}
---
+++ 2.3 spring rest handson/spring-learn/src/main/java/com/cognizant/spring_learn/service/CountryService.java
@@ -0,0 +1,24 @@
+package com.cognizant.spring_learn.service;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.spring_learn.Country;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        @SuppressWarnings("unchecked")
+        List<Country> countryList = (List<Country>) context.getBean("countryList");
+        
+        return countryList.stream()
+                .filter(country -> country.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null);
+    }
+}
